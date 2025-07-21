5a8a23d5af0efb2c596b25442118e50f86c36770
Kojirou <54878605+KameiKojirou@users.noreply.github.com>
2025-07-21 07:45:30 -0600

    static and response updates

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,14 @@ import path from "path";
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handlerReadiness);
+
+function handlerReadiness(req: any, res: any) {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.sendStatus(200).send("OK");
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
